ca755ac45830bbd5b603ee83b567287778abc40e
TrinhVanSang03 <sangtvhe172283@fpt.edu.vn>
2025-10-06 21:55:21 +0700

    fix conflic

--- FrontEnd/src/pages/manager/ClassManage/index.js
+++ FrontEnd/src/pages/manager/ClassManage/index.js
@@ -1,5 +1,5 @@
-import ManagerLayout from "../layouts/manager-layout";
 import ClassList from "./ClassList";
+import ManagerLayout from "../../layouts/manager-layout";
 
 const ClassPage = () => {
   return (
